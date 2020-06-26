d0e4ac12aeab968136c821eb0e5e5bf31b0ef11f
NeonCrafter13 <62461337+NeonCrafter13@users.noreply.github.com>
2020-06-26 22:21:32 +0200

    Added OS Paths

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 # Entrypoint to the Aplication
 import configparser
-
+import os
 
 
 config = configparser.ConfigParser()
-config.read('../settings.ini')
+config.read(os.path.abspath("../settings.ini"))
 print(config.sections())
 print(config['DIRECTORY']['externalMods'])
