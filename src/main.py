5c6acc6a26a97af03f7e6653327953fb06cf5977
NeonCrafter13 <62461337+NeonCrafter13@users.noreply.github.com>
2020-06-27 18:33:46 +0200

    Tried using pyqt

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 # Entrypoint to the Aplication
 import configparser
-import os
+import os,sys
 import mod_finder
+
+from PyQt5.QtWidgets import QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QApplication
 
 config = configparser.ConfigParser()
 config.read("settings.ini")
@@ -9,4 +11,44 @@ config.read("settings.ini")
 externalModsDirectory = os.path.normpath(config['DIRECTORY']['externalMods'])
 steamModsDirectory = os.path.normpath(config["DIRECTORY"]["steamMods"])
 
-print(mod_finder.getAllMods(externalModsDirectory, steamModsDirectory)[0])
+Mods = mod_finder.getAllMods(externalModsDirectory, steamModsDirectory)[0]
+
+class Window(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        okButton = QPushButton("OK")
+        cancelButton = QPushButton("Cancel")
+
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(okButton)
+        hbox.addWidget(cancelButton)
+
+        self.vbox = QVBoxLayout()
+        self.vbox.addStretch(1)
+        self.vbox.addLayout(hbox)
+
+        self.setLayout(self.vbox)
+
+        self.setGeometry(300, 300, 300, 150)
+        self.setWindowTitle('Tpf2 NeonModManager')
+        self.show()
+
+    def displayMods(self):
+        for mod in Mods:
+            name = QLabel(mod.name, self)
+            self.vbox.addWidget(name, 3, 1, 5, 1)
+def main():
+    app = QApplication(sys.argv)
+    ex = Window()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
