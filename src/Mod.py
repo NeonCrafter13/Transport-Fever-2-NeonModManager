9bd3cef483017771e939744024c5c0f909031b02
NeonCrafter13 <62461337+NeonCrafter13@users.noreply.github.com>
2020-06-28 11:08:03 +0200

    Added Export Modlist

--- src/Mod.py
+++ src/Mod.py
@@ -1,9 +1,10 @@
 
 class Mod():
-    def __init__(self, name, minorVersion, source, image, options, location):
+    def __init__(self, name, minorVersion, source, image, options, location, authors="test"):
         self.name = name
         self.minorVersion = minorVersion
         self.source = source
         self.image = image
         self.options = options
         self.location = location
+        self.authors = authors
--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 # Entrypoint to the Aplication
 import configparser
 import os,sys
-import mod_finder
+import mod_finder, modlist
 
-from PyQt5.QtWidgets import QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QApplication
+from PyQt5.QtWidgets import QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QApplication, QMainWindow, QAction
 from PyQt5.QtGui import QIcon
 
 config = configparser.ConfigParser()
@@ -15,18 +15,38 @@ steamModsDirectory = os.path.normpath(config["DIRECTORY"]["steamMods"])
 Mods = mod_finder.getAllMods(externalModsDirectory, steamModsDirectory)
 app = QApplication(sys.argv)
 
-class Window(QWidget):
+class Window(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initMe()
 
     def initMe(self):
+
+        ExportModlist = QAction("export modlist",self)
+        ExportModlist.setShortcut("Ctrl+E")
+        ExportModlist.setStatusTip("export modlist")
+        ExportModlist.triggered.connect(self.export_modlist)
+
+        ImportModlist = QAction("import modlist",self)
+        ImportModlist.setShortcut("Ctrl+O")
+        ImportModlist.setStatusTip("import modlist")
+        ImportModlist.triggered.connect(self.import_modlist)
+
+        menubar = self.menuBar()
+        file = menubar.addMenu("File")
+        file.addAction(ExportModlist)
+        file.addAction(ImportModlist)
+
         self.setGeometry(50,50,500,500)
         self.setWindowTitle("Tpf2 NeonModManager")
         # w.setWindowIcon(QIcon("test.png"))
         self.show()
 
+    def export_modlist(self):
+        modlist.export_modlist(Mods)
 
+    def import_modlist(self):
+        modlist.import_modlist(Mods)
 
 w = Window()
 
