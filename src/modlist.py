7acd706c07006088973ae8f1aa50fbad47afa8bd
NeonCrafter13 <62461337+NeonCrafter13@users.noreply.github.com>
2020-07-06 12:07:37 +0200

    Moved modlist.json

--- src/modlist.py
+++ src/modlist.py
@@ -5,7 +5,7 @@ def export_modlist(mods):
     for mod in mods:
         mod_json = {"name": mod.name, "source": mod.source, "authors": mod.authors}
         mods_json.append(mod_json)
-    json_file = open("../modlist.json", "w")
+    json_file = open("modlist.json", "w")
     json_file.write(json.dumps(mods_json))
 
 def import_modlist(mods):
